c86d9db094a4a041aa356cbecb5adceade005011
Juan José García <39605602+JuanJo193@users.noreply.github.com>
2018-07-19 20:11:16 -0500

    r4t
    86 d

---
+++ Cuenta.java
@@ -0,0 +1,32 @@
+
+package bancojuan;
+
+
+public class Cuenta {
+    
+    private String titular;
+    private float cantidad;
+    private double dinero;
+    
+    public Cuenta(String nombre,float dinero){
+        titular=nombre;
+        cantidad=dinero;
+}
+    public String getTitular(){
+        return titular;
+    }
+    public void setnombre(String nombre){
+     titular=nombre;
+             }
+    public float getcantidad(){
+        return cantidad;
+    }
+    public void setndinero(float dinero){
+     cantidad=dinero;
+             }
+    public void ingresar (double dinero){
+    if 
+        
+    }
+}
+}    
